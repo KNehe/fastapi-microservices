35dde9b7dd0db8385551feee73f7bddb9f3b2d46
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2026-02-24 14:18:40 +0300

    Code for day-3 tutorial

--- services/user-service/main.py
+++ services/user-service/main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from contextlib import asynccontextmanager
+from db import engine, get_session
+from sqlmodel import SQLModel, Session, select
+from models import User, UserPublic
 
-app = FastAPI()
 
-@app.get("/health")
-def health_check():
-    return {"status": "ok", "service": "user-service"}
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+@app.post("/users/", response_model=UserPublic)
+def create_user(user: User, session: Session = Depends(get_session)):
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
+
+@app.get("/users/")
+def read_users(session: Session = Depends(get_session)):
+    users = session.exec(select(User)).all()
+    return users
